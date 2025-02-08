6f999211ace47a26777d686ac6004a1969955d23
Diogo CS Berner <diogoberner@gmail.com>
2025-02-07 23:45:40 -0300

    nav buttons js started

--- app/main.js
+++ app/main.js
@@ -1,5 +1,11 @@
 import showBooks from "./showBooks.js"
-
 const url = "https://guilhermeonrails.github.io/casadocodigo/livros.json"
-
+const navButtons = document.querySelectorAll(".nav .btn")
 showBooks(url)
+
+navButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        const bookType = e.target.value
+        console.log(e.target.value)
+    })
+})
